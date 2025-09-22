77bb43763f12a0d19ed09c8ca27898ed6eaa4bbe
Henrique Dummer <hatiehique2005@gmail.com>
2025-09-22 09:03:15 -0300

    Get flight details working

--- server/src/controllers/flights.controllers.ts
+++ server/src/controllers/flights.controllers.ts
@@ -1,11 +1,11 @@
 import type { Request, Response } from "express";
 import { readDbData } from "../utils/readDbData.ts";
-import type { DB } from "../types/types.ts";
+import type { DB, Flight } from "../types/types.ts";
 
 export const getFlights = (req: Request, res: Response): void => {
   try {
     const db = readDbData<DB>();
-    console.log(db.flights)
+
     const flights = db.flights.map((flight) => ({
       id: flight.id,
       aircraft: flight.aircraft.name,
@@ -14,7 +14,8 @@ export const getFlights = (req: Request, res: Response): void => {
       date: flight.flightData.date,
     }));
 
-    res.status(200).json(flights)
+    res.status(200).json(flights);
+
   } catch (error) {
     console.log(error);
     throw new Error("Failed to get flights");
@@ -23,9 +24,19 @@ export const getFlights = (req: Request, res: Response): void => {
 
 export const getFlightDetails = (req: Request, res: Response): void => {
   try {
-    res.json("Fligth details");
+    const db = readDbData<DB>();
+    const { id } = req.params;
+
+    const flight: Flight | undefined = db.flights.find((f) => f.id === id);
+
+    if (!flight) {
+      res.status(404).json({ message: "Flight not found" });
+    }
+
+    res.json(flight)
+
   } catch (error) {
     console.log(error);
-    throw new Error("Failed to get flights");
+    throw new Error("Failed to get flight");
   }
 };
