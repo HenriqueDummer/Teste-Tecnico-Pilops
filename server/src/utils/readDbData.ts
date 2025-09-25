4f1e3a0a7d5ab3bd79ab4cef4cad92562b2c5563
Henrique Dummer <hatiehique2005@gmail.com>
2025-09-25 18:34:34 -0300

    Moved DB outside src

--- server/src/utils/readDbData.ts
+++ server/src/utils/readDbData.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const DBFile = path.join(__dirname, "../DB/flights.json");
+const DBFile = path.join(__dirname, "../../DB/flights.json");
 
 export function readDbData<T>(): T {
   const data = fs.readFileSync(DBFile, "utf-8");
